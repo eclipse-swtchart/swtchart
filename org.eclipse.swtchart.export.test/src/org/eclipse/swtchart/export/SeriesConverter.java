c4c7df4ed67990be655a4abac3501cabb22575a0
Александър Куртаков <akurtakov@gmail.com>
2026-01-26 14:02:53 +0200

    Streamline tests helper code
    Use try-with-resources and Java NIO

--- org.eclipse.swtchart.export.test/src/org/eclipse/swtchart/export/SeriesConverter.java
+++ org.eclipse.swtchart.export.test/src/org/eclipse/swtchart/export/SeriesConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2017, 2025 Lablicate GmbH.
+ * Copyright (c) 2017, 2026 Lablicate GmbH.
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -13,9 +13,8 @@
 package org.eclipse.swtchart.export;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,11 +29,10 @@ public class SeriesConverter {
 		double[] xSeries = new double[size];
 		double[] ySeries = new double[size];
 
-		BufferedReader bufferedReader = null;
-		try {
+		try (BufferedReader bufferedReader = Files.newBufferedReader(Path.of(file))) {
 			String line;
 			int i = 0;
-			bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			
 			while((line = bufferedReader.readLine()) != null) {
 				if(!line.startsWith("#")) {
 					String[] values = line.split("\t");
@@ -45,14 +43,6 @@ public class SeriesConverter {
 			}
 		} catch(Exception e) {
 
-		} finally {
-			if(bufferedReader != null) {
-				try {
-					bufferedReader.close();
-				} catch(IOException e) {
-
-				}
-			}
 		}
 
 		ISeriesData seriesData = new SeriesData(xSeries, ySeries, file);
@@ -64,24 +54,14 @@ public class SeriesConverter {
 		int size = getNumberOfLines(fileName);
 		double[] ySeries = new double[size];
 
-		BufferedReader bufferedReader = null;
-		try {
+		try (BufferedReader bufferedReader = Files.newBufferedReader(Path.of(fileName))) {
 			String line;
 			int i = 0;
-			bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
 			while((line = bufferedReader.readLine()) != null) {
 				ySeries[i++] = Double.parseDouble(line.trim());
 			}
 		} catch(Exception e) {
 
-		} finally {
-			if(bufferedReader != null) {
-				try {
-					bufferedReader.close();
-				} catch(IOException e) {
-
-				}
-			}
 		}
 
 		ISeriesData seriesData = new SeriesData(ySeries, fileName);
@@ -92,10 +72,8 @@ public class SeriesConverter {
 
 		List<ISeriesData> scatterSeriesList = new ArrayList<>();
 
-		BufferedReader bufferedReader = null;
-		try {
+		try (BufferedReader bufferedReader = Files.newBufferedReader(Path.of(fileName))) {
 			String line;
-			bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
 			while((line = bufferedReader.readLine()) != null) {
 				String[] values = line.split("\t");
 				String id = values[0].trim();
@@ -106,14 +84,6 @@ public class SeriesConverter {
 			}
 		} catch(Exception e) {
 
-		} finally {
-			if(bufferedReader != null) {
-				try {
-					bufferedReader.close();
-				} catch(IOException e) {
-
-				}
-			}
 		}
 		return scatterSeriesList;
 	}
@@ -121,22 +91,12 @@ public class SeriesConverter {
 	private static int getNumberOfLines(String file) {
 
 		int i = 0;
-		BufferedReader bufferedReader = null;
-		try {
-			bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		try (BufferedReader bufferedReader = Files.newBufferedReader(Path.of(file))) {
 			while((bufferedReader.readLine()) != null) {
 				i++;
 			}
 		} catch(Exception e) {
 
-		} finally {
-			if(bufferedReader != null) {
-				try {
-					bufferedReader.close();
-				} catch(IOException e) {
-
-				}
-			}
 		}
 		return i;
 	}
